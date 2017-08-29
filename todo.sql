5dc534cfda73a270387ba610cb30bc320e7c5979
Collin Miller <crmiller10@gmail.com>
2017-08-28 21:30:43 -0400

    Added Update todos and delete todos

--- todo.sql
+++ todo.sql
@@ -1,28 +1,24 @@
--- CREATE TABLE todos (
---   id SERIAL PRIMARY KEY,
---   title VARCHAR(255) NOT NULL,
---   details VARCHAR(100) NULL,
---   priority INTEGER NOT NULL DEFAULT '1',
---   created_at timestamp [ (4) ] [ without time zone ] DEFAULT current_timestamp NOT NULL,
---   completed_at timestamp DEFAULT current_timestamp NULL
--- );
+
 CREATE TABLE todos (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
-  details VARCHAR(100) NULL,
+  details TEXT NULL,
   priority INTEGER NOT NULL DEFAULT '1',
-  created_at timestamp DEFAULT current_timestamp NOT NULL,
-  completed_at timestamp NULL
+  created_at TIMESTAMP NOT NULL,
+  completed_at TIMESTAMP NULL
 );
-INSERT INTO todos (title, priority) VALUES
-('Clean Car', '1'),
-('Do Laundry', '2'),
-('Clean Room', '3'),
-('Mow Lawn', '4'),
-('Grocery Shop', '5')
+INSERT INTO todos (title, details, priority, created_at, completed_at) VALUES
+('Clean car', 'Lorem clean car...', '1', '2017-08-27 04:05:06', NULL),
+('Do laundry', 'Lorem do laundry...', '1', '2017-08-28 02:01:23', NULL),
+('Clean room', 'Lorem clean room...', '3', '2017-08-27 14:06:11', NULL),
+('Mow lawn', 'Lorem mow lawn...', '4', '2017-08-28 09:13:02', NULL),
+('Get groceries', 'Lorem get groceries...', '5', '2017-08-27 06:25:43', '2017-08-28 10:23:54')
 ;
-SELECT * FROM todos;
 
 SELECT * FROM todos WHERE completed_at IS NULL;
 
 SELECT * FROM todos WHERE priority > 1;
+
+UPDATE todos SET completed_at = '2017-08-28 12:00:00' WHERE id = 3;
+
+DELETE FROM todos WHERE completed_at IS NOT NULL;
